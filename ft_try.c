bdcb2426f4ea13816204c84b6e9c999901aee5cc
ninooy <younjw813@korea.ac.kr>
2023-07-15 18:13:41 +0900

    first-commit-mac

--- ft_try.c
+++ ft_try.c
@@ -11,7 +11,7 @@ int main()
     printf(" a 의 값은 : %d \n", a);
     printf(" *p 의 값은 : %d \n ", *p);
     printf(" p의 값은 : %p \n", p);
-    printf("감사합니다.");
+    printf("감사합니다.^^");
 
     return 0;
 }
